9c441a42fcb0221a4572534f035778bfe0df5472
KeanzKun <75829660+KeanzKun@users.noreply.github.com>
2022-04-07 05:19:12 +0800

    Connected Database, left KQ part
    Left KoonQi part to connect Seat and Points earned

--- cinema/TransactionUpcoming.php
+++ cinema/TransactionUpcoming.php
@@ -1,15 +1,34 @@
 <!DOCTYPE html>
+<?php
+include_once('config.php');
+?>
 
 <html>
 
 <head>
-<?php include_once('config.php');?>
-<?php include('includes/navigation2.php'); ?>
-
+    <?php include 'includes/navigation2.php'; ?>
+    
     <link rel="stylesheet" href="style/mystyle.css">
 </head>
 
+
 <body>
+
+<?php
+
+$id = $_SESSION['member_id'];
+
+// Fetch all announcements data from database
+// $result = mysqli_query($mysqli, "SELECT * FROM transaction ORDER BY transaction_id");
+
+$result = mysqli_query($mysqli, "SELECT T.member_id,T.screening_id,S.screening_date,S.screening_time,M.movie_id,M.movie_name,M.movie_poster, M.movie_duration
+FROM transaction T
+JOIN screening S ON T.screening_id = S.screening_id
+JOIN movie M ON S.movie_id = M.movie_id 
+WHERE member_id = $id
+");
+
+?>
 
     <div class="midBtn">
         <a href="TransactionUpcoming.php">
@@ -20,85 +39,39 @@
             <li class="a"> History</li>
         </a>
     </div>
-    <div class="movieBackground">
+    
+     <?php  
 
-        <table class="GeneratedTable">
-            <tbody>
-                <tr>
-                <img src="image/Transaction history - history.png">
-                </tr>
-                <tr>
-                    <table class="GeneratedTable">
-                        <tr>
-                            <td>Movie Name:</td>
-                            <td>The Batman</td>
-                        </tr>
-                        <tr>
-                            <td>Date:</td>
-                            <td>asd</td>
-                        </tr>
-                        <tr>
-                            <td>Time:</td>
-                            <td>asd</td>
-                        </tr>
-                        <tr>
-                            <td>Seat:</td>
-                            <td>asd</td>
-                        </tr>
-                        <tr>
-                            <td>Duration:</td>
-                            <td>asd</td>
-                        </tr>
-                        <tr>
-                            <td>Points Earned:</td>
-                            <td>asd</td>
-                        </tr>
-                    </table>
-                </tr>
-            </tbody>
-        </table>
-        <img src="image/18 Rating.png" class="Rating">
-    </div>
+while($row = mysqli_fetch_array($result)) {  
 
-    <div class="movieBackground">
+    if($row['screening_date'] < date("YYYY/mm/dd")){
+    echo '<div class="movieBackground">';
+    echo '<img src="image/'. $row['movie_poster'] . '">';
+    echo '<table class="GeneratedTable">';
+    echo '<tr>';
+    echo '<td>Movie Name: <td>'. $row['movie_name'] .'</td></td>';
+    echo '<td>    </td>';
+    echo '</tr>';
+    echo '<tr>';
+    echo '<td>Date: <td>'. $row['screening_date'] .'</td></td>';
+    echo '</tr>';
+    echo '<tr>';
+    echo '<td>Time: <td>'. $row['screening_time'] .'</td></td>';
+    echo '</tr>';
+    echo '<tr>';
+    echo '<td>Seat: <td>'. $row['movie_name'] .'</td></td>';
+    echo '</tr>';
+    echo '<tr>';
+    echo '<td>Duration: <td>'. $row['movie_duration'] .'</td></td>';
+    echo '</tr>';
+    echo '<tr>';
+    echo '<td>Points Earned: <td>'. $row['movie_name'] .'</td></td>';
+    echo '</tr>';
+    echo '</table>';
 
-<table class="GeneratedTable">
-    <tbody>
-        <tr>
-        <img src="image/Transaction history - history.png">
-        </tr>
-        <tr>
-            <table class="GeneratedTable">
-                <tr>
-                    <td>Movie Name:</td>
-                    <td>The Batman</td>
-                </tr>
-                <tr>
-                    <td>Date:</td>
-                    <td>asd</td>
-                </tr>
-                <tr>
-                    <td>Time:</td>
-                    <td>asd</td>
-                </tr>
-                <tr>
-                    <td>Seat:</td>
-                    <td>asd</td>
-                </tr>
-                <tr>
-                    <td>Duration:</td>
-                    <td>asd</td>
-                </tr>
-                <tr>
-                    <td>Points Earned:</td>
-                    <td>asd</td>
-                </tr>
-            </table>
-        </tr>
-    </tbody>
-</table>
-<img src="image/18 Rating.png" class="Rating">
-</div>
+    echo '</div>';
+}}
+    ?>
 
 </body>
 <?php include 'includes/footer.php'; ?>
